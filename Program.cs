70118fe8e52af34d756dd202b2f08ea17c85eb75
celisej567 <75926135+celisej567@users.noreply.github.com>
2021-07-06 16:22:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1
+{
+
+    public class name 
+    {
+        public string inc(int hin)
+        {
+            return $"{hin}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var inf in args)
+            {
+                Console.WriteLine(inf);
+            } 
+            
+            //Console.WriteLine("c=="+name_.inc(3));
+            Console.ReadLine();
+        }
+    }
+}
